b7279ae91d2bf0f244e35d1f9de06c7b78c69314
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-06-11 21:56:41 +0530

    ..

--- promise_sample.js
+++ promise_sample.js
@@ -1,17 +1,41 @@
-function add(num1,num2,callback){
-    let err=false
-    if(num1==0){
-       err=true
-    }
-    callback(num1+num2,err)
-}
 
-add(0,20,(sum,err)=>{
-    if(err){
-        console.log("first number is 0")
-    }
-    else{
-        console.log(sum)
-    }
-    
+
+const Promise=require("promise")
+
+function add(num1,num2){
+  return new Promise((resolve,reject)=>{
+      if (num1==0){
+          reject("error:first number is zero")
+      }
+      resolve(num1+num2)
+  })
+}
+function multiply(num1,num2){
+    return new Promise((resolve,reject)=>{
+        if (num1==0){
+            reject("error:first number is zero")
+        }
+        resolve(num1*num2)
+    })
+  }
+  function div(num1,num2){
+    return new Promise((resolve,reject)=>{
+        if (num1==0){
+            reject("error:first number is zero")
+        }
+        resolve(num1/num2)
+    })
+  }
+
+add(10,20).then((sum)=>{
+    console.log(sum)
+    return multiply(sum,sum)
+}).then(product=>{
+    console.log(product)
+    return div(product,10)
+}).then(divd=>{
+    console.log(divd)
+})
+.catch((err)=>{
+    console.log(err)
 })
